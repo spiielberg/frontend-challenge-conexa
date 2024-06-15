7ec8c76071aa603651278155a60492c466bf9dae
Spielberg Hanielly <spiielberg@gmail.com>
2024-06-15 18:14:37 -0300

    fix: prevent user data from being cached

--- src/data/user.ts
+++ src/data/user.ts
@@ -3,11 +3,15 @@
 import { User } from '@/types/user'
 
 export const getUser = async () => {
-  const response = await fetch('http://localhost:3333/user')
+  const response = await fetch('http://localhost:3333/user', {
+    cache: 'no-store',
+  })
 
   if (!response.ok) {
     return null
   }
 
-  return (await response.json()) as User
+  const user = (await response.json()) as User
+
+  return user
 }
